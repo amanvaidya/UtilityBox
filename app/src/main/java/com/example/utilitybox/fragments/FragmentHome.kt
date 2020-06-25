2cc328934f527b44a6864a4d630cf972e7687f53
amanvaidya <amanvaidya700@gmail.com>
2020-06-25 22:14:56 +0530

    /* *working set google maps. *implemented google maps inside fragment. */

--- app/src/main/java/com/example/utilitybox/fragments/FragmentHome.kt
+++ app/src/main/java/com/example/utilitybox/fragments/FragmentHome.kt
@@ -1,7 +1,9 @@
 package com.example.utilitybox.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -12,35 +14,46 @@ import com.example.utilitybox.R
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException
 import com.google.android.gms.common.GooglePlayServicesUtil
-import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
 
-class FragmentHome : Fragment() {
-    var mapView: MapView? = null
-    var map: GoogleMap? = null
-    var v: View?= null
-   override fun onCreateView(
+class FragmentHome : Fragment(), OnMapReadyCallback {
+
+
+    val position = LatLng(-33.920455, 18.466941)
+    var v:View?=null
+    var markerOptions = MarkerOptions().position(position)
+    private lateinit var mMap: GoogleMap
+    lateinit var marker : Marker
+    var mapView:MapView? = null
+    override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
        v = inflater.inflate(R.layout.fragment_fragment_home, container, false)
-       try{
-            MapsInitializer.initialize(activity)
-       }catch(e: GooglePlayServicesNotAvailableException){
-           Log.e("Address Map", "Could not initialize google play", e)
-       }
-       when (GooglePlayServicesUtil.isGooglePlayServicesAvailable(getActivity()) )
-       {
-           ConnectionResult.SUCCESS-> Toast.makeText(getActivity(), "SUCCESS", Toast.LENGTH_SHORT).show()
-           mapView = v.findViewById<MapView>(R.id.map)
 
-       }
-
-
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(activity)
         return v
     }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+}
+
+
+private fun SupportMapFragment.getMapAsync(activity: FragmentActivity?) {
+
 }

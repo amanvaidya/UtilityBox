9f478ad1dba75726ecac3ec8ea5241a4fa021d57
amanvaidya <amanvaidya700@gmail.com>
2020-07-27 08:39:10 +0530

    /*working set polyline and polygons*/

--- app/src/main/java/com/example/utilitybox/SplashScreen.kt
+++ app/src/main/java/com/example/utilitybox/SplashScreen.kt
@@ -5,14 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
-class MainActivity : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
     private val timeOut:Long=3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //
         Handler().postDelayed({
-           val i = Intent(this@MainActivity, Login::class.java)
+           val i = Intent(this@SplashScreen, Login::class.java)
             startActivity(i)
             finish()
         },timeOut)
